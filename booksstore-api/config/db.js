58cfa0f97f92680e5c021634fb5c6a9f0627a9fa
Nagaraj A <anagarajgouda05@gmail.com>
2025-02-16 10:40:40 +0530

    removing of drivers url from mongodb connection

--- booksstore-api/config/db.js
+++ booksstore-api/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://nodeapi:5ZalaCbWXEOHTSnU@nodeapi-cluster.nwqim.mongodb.net/booksstore-api');
+        const conn = await mongoose.connect('');
         console.log(`MongoDB Connected to ${conn.connection.host}`);
     } catch(error){
         console.log(error);
